48c33e0f89d6bf4627ba5926074f4c638c17f658
gokce <gokcetascii@gmail.com>
2024-02-06 14:56:27 +0300

    Added flight details depending on whether one way is selected or not

--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FlightList = ({ searchResults }) => {
+const FlightList = ({ searchResults, oneWay }) => {
   return (
     <div>
       {searchResults.length > 0 && (
@@ -9,7 +9,30 @@ const FlightList = ({ searchResults }) => {
           <ul>
             {searchResults.map((flight, index) => (
               <li key={index}>
-                {`Departure Airport: ${flight.departureAirport}, Arrival Airport: ${flight.arrivalAirport}, Price: ${flight.details.price}, Duration: ${flight.details.flightDuration}`}
+                <p>Departure Airport: {flight.departureAirport}</p>
+                <p>Arrival Airport: {flight.arrivalAirport}</p>
+                {!oneWay && flight.returnDate && (
+                  <>
+                    <p>Departure Date: {flight.departureDate}, Return Date: {flight.returnDate}</p>
+                    <p>Departure Time 1: {flight.details.firstDepartureTime}</p>
+                    <p>Arrival Time 1: {flight.details.firstArrivalTime}</p>
+                    <p>Flight Duration 1: {flight.details.firstFlightDuration}</p>
+                    <p>Price 1: {flight.details.firstPrice}</p>
+                    <p>Departure Time 2: {flight.details.secondDepartureTime}</p>
+                    <p>Arrival Time 2: {flight.details.secondArrivalTime}</p>
+                    <p>Flight Duration 2: {flight.details.secondFlightDuration}</p>
+                    <p>Price 2: {flight.details.secondPrice}</p>
+                  </>
+                )}
+                {!oneWay && !flight.returnDate && (
+                  <>
+                    <p>Departure Date: {flight.departureDate}</p>
+                    <p>Departure Time: {flight.details.firstDepartureTime}</p>
+                    <p>Arrival Time: {flight.details.firstArrivalTime}</p>
+                    <p>Flight Duration: {flight.details.firstFlightDuration}</p>
+                    <p>Price: {flight.details.firstPrice}</p>
+                  </>
+                )}
               </li>
             ))}
           </ul>
