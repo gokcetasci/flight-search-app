93a120f4490fd6af1faa332a7e81357c5d9acca8
gokce <gokcetascii@gmail.com>
2024-02-07 01:39:10 +0300

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Search from './components/Search';
-import FlightList from './components/FlightList';
+import Homepage from './components/Homepage';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Search />} />
-        <Route
-          path="/flight-results"
-          element={<FlightList />}
-        />
+        <Route path="/" element={<Homepage />} />
+       
       </Routes>
     </Router>
   );
