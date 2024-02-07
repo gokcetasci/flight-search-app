ea74febd5729557031633a9927fb611fe0eb18ab
gokce <gokcetascii@gmail.com>
2024-02-07 23:39:20 +0300

    deleted icons

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Search from "./Search";
 import img1 from "../images/img1.jpg"
 import Footer from "./Footer";
-import { GiRingedPlanet } from "react-icons/gi";
 
 function Homepage() {
   return (
@@ -13,12 +12,7 @@ function Homepage() {
       backgroundRepeat: "repeat-y", // Yatay eksende tekrarlama
 
     }}>
-      <div className="absolute top-5 left-10 flex flex-row items-center justify-center ">
-            <GiRingedPlanet className="w-10 h-10 fill-cyan-400" />
-            <span className="font-bold italic  text-cyan-500 text-[32px]">
-              F<span className="text-[26px]">LIGHT</span>
-            </span>
-          </div>
+      
       <div className="flex items-center justify-center p-20 ">
         <Search />
       </div>
