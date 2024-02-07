3c2d3123d7959ffa42387f185c256b7680d9d5b9
gokce <gokcetascii@gmail.com>
2024-02-07 15:19:05 +0300

    loading spinner page created and content added

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+// LoadingSpinner.js
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+    <div className="spinner"></div>
+    Loading...
+  </div>
+  );
+};
+
+export default LoadingSpinner;
