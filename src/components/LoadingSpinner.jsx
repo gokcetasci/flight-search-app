67a5bccf69035725ea4348170e9a35745183ea9a
gokce <gokcetascii@gmail.com>
2024-02-07 16:34:49 +0300

    styles added

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const LoadingSpinner = () => {
   return (
     <div className="loading-spinner">
-    <div className="spinner"></div>
-    Loading...
-  </div>
+      <div className="spinner"></div>
+      <p className="text-white">Loading...</p>
+    </div>
   );
 };
 
