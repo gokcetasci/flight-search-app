1028b0c4839b130ea048b759386ffb18765299ea
gokce <gokcetascii@gmail.com>
2024-02-19 13:46:30 +0300

    added responsive and website!s logo, background image's location changed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import Search from "./Search";
-import img1 from "../images/img1.jpg"
+import img1 from "../images/img1.jpg";
 import Footer from "./Footer";
+import { GiRingedPlanet } from "react-icons/gi";
 
 function Homepage() {
   return (
-    <div  className="homepage-container items-center justify-center relative bg-blue-600 w-full h-screen"
-    style={{
-      backgroundImage: `url(${img1})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "repeat-y", // Yatay eksende tekrarlama
-
-    }}>
-      
-      <div className="flex items-center justify-center p-20 ">
+    <div className="homepage-container items-center justify-center relative w-full h-screen">
+      <div className="flex flex-row items-center justify-center absolute top-4 left-10">
+          <GiRingedPlanet className="w-10 h-10 fill-cyan-400" />
+          <span className="font-bold italic text-[#18163e] text-cyan-500 text-[32px]">
+            F<span className="text-[26px]">LIGHT</span>
+          </span>
+        </div>
+      <div
+        className="flex items-center justify-center p-20 "
+        style={{
+          backgroundImage: `url(${img1})`,
+          backgroundSize: "cover",
+        }}
+      >
         <Search />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
